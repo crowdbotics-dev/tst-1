30be5160ed64e2e1caa745e3fdf5b8ba96ef46fa
Crowdbotics <engineering@crowdbotics.com>
2021-08-16 14:18:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "tst_1Identifier",
+  url: "https://tst_1Identifier.botics.co",
+  api: "https://tst_1Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
